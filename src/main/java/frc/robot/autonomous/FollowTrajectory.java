07721206b763663d4e202206d557339fb26f6cbb
sammulvey747 <sam.mulvey747@gmail.com>
2022-05-31 22:57:49 -0400

    B0.3.1SM - PathPlanner Added
    I took some time to convert the FollowTrajectory command group to be usable for any path you want. Using the PathPlanner library and code from  @MC42 and @Squid5678, I created a simple implementation of a class that can handle "any" path.
    
    Auto still needs to be tuned and none of this was actually tested (I have some doubts about if the robot will be able to find the path based on how I typed it), but overall this isn't a huge deviation from what I saw working in person today. The path the robot will drive is a 2 meter drive straight forward. I assume we have to tune the 3 PID controllers independently, so I think three different paths for tuning is appropriate. Thought I would write this at home so we didn't waste time during the next meeting.

--- src/main/java/frc/robot/autonomous/FollowTrajectory.java
+++ src/main/java/frc/robot/autonomous/FollowTrajectory.java
@@ -4,16 +4,15 @@
 
 package frc.robot.autonomous;
 
-import java.util.List;
+
+import java.nio.file.Path;
+
+import com.pathplanner.lib.PathPlanner;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -26,46 +25,41 @@ import frc.robot.subsystems.SwerveSubsystem;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class FollowTrajectory extends SequentialCommandGroup {
 
-  SwerveSubsystem SWERVE_SUBSYSTEM = new SwerveSubsystem();
+  // Declare swerve subsystem
+  SwerveSubsystem SWERVE_SUBSYSTEM;
 
   /** Creates a new FollowTrajectory. */
-  public FollowTrajectory(SwerveSubsystem swerve) {
+  public FollowTrajectory(SwerveSubsystem swerve, String path) {
 
+    // Find the exact filepath of the file on the roboRio given the path we want to access
+    Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(path);
+
+    // Set our local swerve subsystem to be equal to the one that was passed in, so we can access all of the same motors
     this.SWERVE_SUBSYSTEM = swerve;
 
-    // Create trajectory settings
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
-        AutoConstants.kMaxSpeedMetersPerSecond,
-        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        .setKinematics(DriveConstants.kDriveKinematics);
+    // Load a trajectory fron the specified path and convert it to a WPILib trajectory
+    Trajectory loaded_trajectory = PathPlanner.loadPath(
+          trajectoryPath.toString(), 
+          AutoConstants.kMaxSpeedMetersPerSecond, 
+          AutoConstants.kMaxAccelerationMetersPerSecondSquared
+    );
 
-    // Generate Trajectory
-    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(0, 0, new Rotation2d(0)),
-        List.of(
-          new Translation2d(1, 0),
-          new Translation2d(1, -1)
-        ),
-        new Pose2d(2, -1, Rotation2d.fromDegrees(90)),
-        trajectoryConfig
-        );
-
-    Trajectory trajectory2 = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(0, 0, new Rotation2d(0)),
-        List.of(), 
-        new Pose2d(2, 0, new Rotation2d(0)), 
-        trajectoryConfig
-     );
-
-
+    // Instantiate the x and y PID controllers. They operate indepentently (Holomonic system)
     PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
     PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
-    ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, 
+
+    // This PID controller is a little bit different as we want to restrict some of the outputs. We tell 
+    // it that we have a defined max angular velocity and acceleration.
+    ProfiledPIDController thetaController = new ProfiledPIDController(
+                AutoConstants.kPThetaController, 0, 0, 
                 AutoConstants.kThetaControllerConstraints);
+
+    // Allow the robot to realize the wheels can spin in a full circle, allowing for jumps from max and min bounds
     thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
+    // Instantiate the SwerveControllerCommand where we pass in all the stuff we defined and it will follow the trajectory
     SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-      trajectory2,
+      loaded_trajectory,
       SWERVE_SUBSYSTEM::getPose,
       DriveConstants.kDriveKinematics,
       xController,
@@ -79,8 +73,34 @@ public class FollowTrajectory extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new InstantCommand(() -> SWERVE_SUBSYSTEM.resetOdometry(trajectory.getInitialPose())), 
+      new InstantCommand(() -> SWERVE_SUBSYSTEM.resetOdometry(loaded_trajectory.getInitialPose())), 
       swerveControllerCommand, 
       new InstantCommand(() -> SWERVE_SUBSYSTEM.stopModules()));
   }
 }
+
+
+
+
+
+// Storing these things at the end of the file in case they are needed for the future.
+// This is the manual way to make trajectories in WPILib. Trajectory config is used
+// to define the rotational constraints
+
+    // Create trajectory settings
+    // TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
+    //     AutoConstants.kMaxSpeedMetersPerSecond,
+    //     AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+    //     .setKinematics(DriveConstants.kDriveKinematics
+    //     );
+
+    // Generate Trajectory
+    // Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+    //     new Pose2d(0, 0, new Rotation2d(0)),
+    //     List.of(
+    //       new Translation2d(1, 0),
+    //       new Translation2d(1, -1)
+    //     ),
+    //     new Pose2d(2, -1, Rotation2d.fromDegrees(90)),
+    //     trajectoryConfig
+    //     );
